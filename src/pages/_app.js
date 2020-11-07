d816542df790ceec3dfe45f0f4f057766c55a98f
Steve Lee <qwersteve07@gmail.com>
2020-11-08 01:34:57 +0800

    contact

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -43,7 +43,7 @@ const App = ({ Component, pageProps }) => {
   return (
     <>
       <Head>
-        <meta charset="utf-8" />
+        <meta charSet="utf-8" />
         <meta
           name="viewport"
           content="width=device-width,initial-scale=1, viewport-fit=cover"
