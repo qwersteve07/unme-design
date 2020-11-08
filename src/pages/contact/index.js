99038eabcf38bcdba5cff166f8223eed0cec5cb1
qwersteve07 <qwersteve07@gmail.com>
2020-11-08 21:48:02 +0800

    contact form adjsut

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -1,22 +1,65 @@
-import dynamic from "next/dynamic";
-import React from "react";
-import styles from "pages/about/index.module.sass";
+import React, { useState } from "react";
+import styles from "pages/contact/index.module.sass";
 import PageContainer from "components/page-container";
-import Tags from "components/tags";
-import wolf from "images/wolf.jpg";
-import elephant from "images/elephant.jpg";
-import dog from "images/dog.jpg";
-import jellyfish from "images/jellyfish.jpg";
-import project01 from "images/project01.jpg";
-import project02 from "images/project02.jpg";
+import emailjs from "emailjs-com";
+import Arrow from "components/arrow";
 
-const Carousel = dynamic(() => import("components/carousel"), { ssr: false });
+const Contact = () => {
+  const [name, setName] = useState("");
+  const [company, setCompany] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [caseType, setCaseType] = useState("brand-domain-design");
+  const [comment, setComment] = useState("");
 
-const Contact = ({ memberData, projectsData }) => {
+  const caseData = [
+    {
+      key: "brand-domain-design",
+      value: "Brand Domain Design 品牌空間設計",
+    },
+    {
+      key: "interior-design",
+      value: "Interior Design 空間規劃設計",
+    },
+    {
+      key: "idendity-design",
+      value: "Idendity Design 品牌識別設計",
+    },
+    {
+      key: "consulting-cooperation",
+      value: "Consulting and Cooperation 顧問諮詢與合作",
+    },
+  ];
+
+  const sendMail = (e) => {
+    e.preventDefault();
+    let params = {
+      name,
+      company,
+      email,
+      phone,
+      caseType: caseData.find((x) => x.key === caseType).value,
+      comment,
+    };
+
+    let serviceId = "unme";
+    let templateId = "template_408xf4h";
+    let userID = "user_4mY5wOOGGrgwMptW1b5zE";
+    emailjs
+      .send(serviceId, templateId, params, userID)
+      .then((response) => {
+        console.log(response);
+        console.log("SUCCESS!", response.status, response.text);
+      })
+      .catch((error) => {
+        console.log(error);
+        console.log("FAILED...", error);
+      });
+  };
   return (
     <PageContainer>
       <div className={styles.contact}>
-        <div classNaem={styles.desc}>
+        <div className={styles.desc}>
           非我設計運用斜槓與創新的技能，將一切內部作業線上化，
           省下的空間經營成本，讓夥伴吃得更胖，合作CP值變高！
           <br />
@@ -28,8 +71,9 @@ const Contact = ({ memberData, projectsData }) => {
             <input
               id="name"
               type="text"
-              value=""
+              value={name}
               placeholder="請輸入稱呼名字"
+              onChange={(e) => setName(e.target.value)}
             />
           </fieldset>
           <fieldset>
@@ -37,8 +81,9 @@ const Contact = ({ memberData, projectsData }) => {
             <input
               id="company"
               type="text"
-              value=""
+              value={company}
               placeholder="請輸入公司名稱"
+              onChange={(e) => setCompany(e.target.value)}
             />
           </fieldset>
           <fieldset>
@@ -47,8 +92,9 @@ const Contact = ({ memberData, projectsData }) => {
               id="email"
               type="email"
               inputMode="email"
-              value=""
+              value={email}
               placeholder="請輸入信箱"
+              onChange={(e) => setEmail(e.target.value)}
             />
           </fieldset>
           <fieldset>
@@ -56,44 +102,35 @@ const Contact = ({ memberData, projectsData }) => {
             <input
               id="phone"
               type="tel"
-              value=""
+              value={phone}
               placeholder="請輸入聯絡電話"
+              onChange={(e) => setPhone(e.target.value)}
             />
           </fieldset>
-          <fieldset>
-            <label htmlFor="case">案件類型</label>
-            <select id="case">
-              <option value="brand-design">
-                Brand Domain Design 品牌空間設計
-              </option>
-              <option value="interior-design">
-                Interior Design 空間規劃設計
-              </option>
-              <option value="idendity-design">
-                Idendity Design 品牌識別設計
-              </option>
-              <option value="consulting-cooperation">
-                Consulting and Cooperation 顧問諮詢與合作
-              </option>
+          <fieldset className={styles.select}>
+            <label htmlFor="caseType">案件類型</label>
+            <select id="caseType" onChange={(e) => setCaseType(e.target.value)}>
+              {caseData.map((data) => {
+                return <option value={data.key}>{data.value}</option>;
+              })}
             </select>
           </fieldset>
           <fieldset>
-            <label htmlFor="case">備註</label>
-            <textarea placeholder="歡迎分享更多關於你的事情，讓我們更理解你一點，UnMe動物園將於兩個工作日內，由比較清幽的動物回覆訊息" />
+            <label htmlFor="comment">備註</label>
+            <textarea
+              id="comment"
+              rows="5"
+              width="360"
+              placeholder="歡迎分享更多關於你的事情，讓我們更理解你一點，UnMe動物園將於兩個工作日內，由比較清幽的動物回覆訊息"
+              value={comment}
+              onChange={(e) => setComment(e.target.value)}
+            />
           </fieldset>
+          <Arrow hover={true} />
         </form>
       </div>
     </PageContainer>
   );
 };
-
-export async function getStaticProps() {
-  return {
-    props: {
-      memberData,
-      projectsData,
-    },
-  };
-}
 
 export default Contact;
