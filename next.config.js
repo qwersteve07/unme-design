ac90a450c6846bd135d915ed42b874b52557928f
qwersteve07 <qwersteve07@gmail.com>
2021-04-25 16:04:51 +0800

    upgrade to nextjs 10.1.3

--- next.config.js
+++ next.config.js
@@ -2,5 +2,5 @@ const withPlugins = require("next-compose-plugins");
 const optimizedImages = require("next-optimized-images");
 
 module.exports = withPlugins([[optimizedImages]], {
-  exportTrailingSlash: true,
+  trailingSlash: true,
 });
